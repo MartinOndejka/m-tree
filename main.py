eba4ab2d8219dcd72b07c6c0b8d9ead2495d7383
Martin Ondejka <martin.ondejka@vacuumlabs.com>
2021-05-11 21:58:34 +0200

    add split bugged

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         ]
     )
 
-    print(db)
+    print("asd")
 
 if __name__ == '__main__':
     main()
--- mtree/mtree.py
+++ mtree/mtree.py
@@ -1,5 +1,6 @@
 import math
 from uuid import uuid4
+import random
 
 
 DEFAULT_NODE_SIZE = 5
@@ -11,12 +12,55 @@ def euclidean_distance(a, b):
             for i in range(len(a))
         ]))
 
+def promote_random(all_objects):
+    o1 = random.choice(all_objects)
+    all_objects.remove(o1)
+
+    o2 = random.choice(all_objects)
+    all_objects.remove(o2)
+
+    return o1, o2
+
+def partition_generalized_hyperplane(df):
+    def closure(all, o1, o2):
+        n1 = []
+        n2 = []
+
+        for i in all:
+            o1_dist = df(i.value, o1.value)
+            o2_dist = df(i.value, o2.value)
+
+            if o1_dist < o2_dist:
+                n1.append(i)
+            else:
+                n2.append(i)
+        
+        if not n1:
+            n2 = n1[:int(len(n1)/2)]
+            n1 = n1[int(len(n1)/2):]
+        
+        if not n2:
+            n1 = n2[:int(len(n2)/2)]
+            n2 = n2[int(len(n2)/2):]
+
+        return n1, n2
+    
+    return closure
+
 class MTree:
 
-    def __init__(self, node_size=DEFAULT_NODE_SIZE, df=euclidean_distance):
-        self.root = LeafNode()
+    def __init__(
+        self,
+        node_size=DEFAULT_NODE_SIZE,
+        df=euclidean_distance,
+        promotion_method=promote_random,
+        partiotion_method=None
+    ):
+        self.root = Node()
         self.node_size = node_size
         self.df = df
+        self.promote = promotion_method
+        self.partition = partiotion_method if partiotion_method else partition_generalized_hyperplane(df)
 
     def add(self, obj):
         self._insert(self.root, obj)
@@ -36,17 +80,20 @@ class MTree:
 
         if len(leaf.entries) < self.node_size:
             leaf.entries.append(
-                GroundObject(
-                    obj,
-                    uuid4(),
-                    self.df(leaf.parent.value, obj) if leaf.parent else 0
+                EntryObject(
+                    value=obj,
+                    uid=uuid4(),
+                    p_dist=self.df(leaf.parent.value, obj) if leaf.parent_object else 0
                 )
             )
         else:
             self._split(leaf, obj)
 
+    def is_leaf(self, node):
+        return all([i.subtree is None for i in node.entries])
+
     def _find_leaf(self, node, obj):
-        if isinstance(node, LeafNode):
+        if self.is_leaf(node):
             return node
         
         # (obj , dist)
@@ -67,31 +114,76 @@ class MTree:
 
 
     def _split(self, node, obj):
-        print("SPLIT")
+        parent_object = None
+
+        if node is not self.root:
+            parent_object = node.parent_object
+            parent_node = node.parent_node
+        
+        all_objects = node.entries + [EntryObject(value=obj, p_dist=self.df(parent_object.value, obj) if parent_object else 0)]
+
+        o1, o2 = self.promote(all_objects)
+        n1, n2 = self.partition(all_objects, o1, o2)
+
+        node.entries = n1
+        node.parent_object = o1
+        o1.subtree = node
+
+        new_node = Node(
+            entries=n2,
+            parent_object=o2,
+        )
+        o2.subtree = new_node
+
+        if node is self.root:
+            self.root = Node(
+                entries=[
+                    EntryObject(value=o1.value),
+                    EntryObject(value=o2.value),
+                ],
+            )
+            self._update_node(self.root)
+            node.parent_node = self.root
+            new_node.parent_node = self.root
+        
+        else:
+            parent_node.entries.remove(parent_object)
+            parent_node.entries.append(o1)
+
+            new_node.parent_node = parent_node
+
+            if parent_node.entries < self.node_size:
+                parent_node.entries.append(o2)
+            else:
+                self._split(parent_node, o2.value)
+    
+    def _update_node(self, node):
+        if node.parent_object is None:
+            for i in node.entries:
+                i.p_dist = 0
+        
+        else:
+            node.parent_object.radius = 0
+
+            for i in node.entries:
+                i.p_dist = self.df(i.value, node.parent_object.value)
+                
+                if i.p_dist > node.parent_object.radius:
+                    node.parent_object.radius = i.p_dist
 
 
-class AbstractNode:
-    def __init__(self, entries=None, parent=None):
+
+class Node:
+    def __init__(self, entries=None, parent_object=None, parent_node=None):
         self.entries = entries if entries else []
-        self.parent = parent
-
-class InternalNode(AbstractNode):
-    pass
-
-class LeafNode(AbstractNode):
-    pass
+        self.parent_object = parent_object
+        self.parent_node = parent_node
 
 
-class RoutingObject:
-    def __init__(self, value, radius, p_dist, subtree=None):
+class EntryObject:
+    def __init__(self, value, p_dist=0, radius=None, uid=None, subtree=None):
         self.value = value
         self.radius = radius
         self.p_dist = p_dist
-        self.subtree = subtree if subtree else []
-
-
-class GroundObject:
-    def __init__(self, value, uid, p_dist):
-        self.value = value
         self.uid = uid
-        self.p_dist = p_dist
+        self.subtree = subtree
